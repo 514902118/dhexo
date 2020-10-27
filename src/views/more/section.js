bd609b888d82900c40f61b792605e29b4512061e
514902118@qq.com <514902118@qq.com>
2020-10-27 23:28:07 +0800

    more-title1样式

---
+++ src/views/more/section.js
@@ -0,0 +1,141 @@
+const section1 = [
+  {
+    name: 'ClinVar',
+    type: '变异+疾病',
+    sketch:'变异和人类健康之间的关联关系',
+    language: '英文'
+  },{
+    name: 'dbNSFP',
+    type: '变异',
+    sketch: '非同义突变的功能预测和注释信息',
+    language: '英文'
+  },{
+    name: 'dbSNP',
+    type: '变异',
+    sketch: '1200多万个在外显子区域上的SNP，以及他们所map的gene',
+    language: '英文'
+  },{
+    name: 'FATHMM',
+    type: '变异',
+    sketch: '记录了大量变异是否会造成危害的预测结果',
+    language: '英文'
+  },{
+    name: 'GARD',
+    type: '疾病',
+    sketch: '6000多种罕见病的详细信息，包括概述、症状和诊断等相关信息',
+    language: '英文'
+  },{
+    name: 'genomeAD',
+    type: '变异',
+    sketch: '1200多万个在外显子区域上的SNP的详细信息，包括染色体、位置、ref、alt以及在不同population上的MAF',
+    language: '英文'
+  },{
+    name: 'GRASP',
+    type: '变异+疾病',
+    sketch: '记录了7000多条来自GWAS研究的变异与遗传病之间的关联信息',
+    language: '英文'
+  },{
+    name: 'GWAS catalog',
+    type: '变异+疾病',
+    sketch: '记录了500多条来自GWAS研究的变异与遗传病之间的关联信息',
+    language: '英文'
+  },{
+    name: 'HPO',
+    type: '症状（表型）+基因',
+    sketch: '基因及疾病的概述，以及疾病的各个症状',
+    language: '英文'
+  },{
+    name: 'Human Gene Mutation Database',
+    type: '变异+疾病',
+    sketch: '约30万变异和疾病之间的关联关系',
+    language: '英文'
+  },{
+    name: 'LOVD',
+    type: '变异',
+    sketch: '约20万变异在多种数据库中的信息整合与收集',
+    language: '英文'
+  },{
+    name: 'LRT',
+    type: '变异',
+    sketch: '记录了大量变异是否会造成危害的预测结果',
+    language: '英文'
+  },{
+    name: 'MitoMAP',
+    type: '变异',
+    sketch: '线粒体DNA上变异的相关信息',
+    language: '英文'
+  },{
+    name: 'MutationAssessor',
+    type: '变异',
+    sketch: '记录了大量变异是否会造成危害的预测结果',
+    language: '英文'
+  },{
+    name: 'MutationMaster',
+    type: '变异',
+    sketch: '记录了大量变异是否会造成危害的预测结果',
+    language: '英文'
+  },{
+    name: 'NCBI PubMed abstract',
+    type: '变异+疾病+基因',
+    sketch: '通过NLP处理后提取变异、基因、疾病三者之间的关联关系',
+    language: '英文'
+  },{
+    name: 'NCBI RefSeq',
+    type: '基因',
+    sketch: '16000多个OMIM上基因的基本信息，包括位点、染色体和坐标等',
+    language: '英文'
+  },{
+    name: 'OMIM',
+    type: '变异+疾病',
+    sketch: '9000多种孟德尔遗传病以及16000多个基因的详细信息，包括概述，疾病与基因、变异的关联等诊断相关的信息',
+    language: '英文'
+  },{
+    name: 'ORPHANET',
+    type: '疾病',
+    sketch: '9000多种疾病的详细信息，包括概述和诊断等相关信息',
+    language: '英文'
+  },{
+    name: 'PGG',
+    type: '变异',
+    sketch: '变异	1200多万个在外显子区域上的SNP的详细信息，包括染色体、位置、ref、alt以及在不同population上的MAF',
+    language: '英文'
+  },{
+    name: 'PolyPhen2',
+    type: '变异',
+    sketch: '记录了大量变异是否会造成危害的预测结果',
+    language: '英文'
+  },{
+    name: 'PROVEAN',
+    type: '变异',
+    sketch: '记录了大量变异是否会造成危害的预测结果',
+    language: '英文'
+  },{
+    name: 'SIFT',
+    type: '变异',
+    sketch: '记录了大量变异是否会造成危害的预测结果',
+    language: '英文'
+  },{
+    name: 'Uniprot',
+    type: '蛋白',
+    sketch: '几乎所有蛋白质及其功能等相关信息',
+    language: '英文'
+  },
+]
+
+const section2 = []
+const section3 = []
+const section4 = []
+const section5 = []
+const section6 = []
+const section7 = []
+
+
+export {
+  section1,
+  section2,
+  section3,
+  section4,
+  section5,
+  section6,
+  section7
+}
